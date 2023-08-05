3026b7103dee02917d7a296c291fa6192aa263dc
mahmoudklaled <mahmoud.khaled19640@gmail.com>
2023-08-05 13:51:48 +0300

    Comments

--- Business/Posts/Services/ICommentServices.cs
+++ Business/Posts/Services/ICommentServices.cs
@@ -11,12 +11,14 @@ namespace Business.Posts.Services
     public interface ICommentServices
     {
         Task<bool> DeletePostCommentAsync(Guid commentId, string userEmail);
-        Task<bool> AddPostCommentAsync(AddCommentRequest comment, string userEmail);
+        Task<(bool, Guid)> AddPostCommentAsync(AddCommentRequest comment, string userEmail);
         Task<bool> UpdatePostCommentAsync(CommentUpdateRequest comment, string userEmail);
         Task<bool> DeleteQuestionCommentAsync(Guid commentId, string userEmail);
-        Task<bool> AddQuestionCommentAsync(AddCommentRequest comment, string userEmail);
+        Task<(bool, Guid)> AddQuestionCommentAsync(AddCommentRequest comment, string userEmail);
         Task<bool> UpdateQuestionCommentAsync(CommentUpdateRequest comment, string userEmail);
         Task<List<PostComment>> GetPostCommentsAsync(Guid postId ,int cntToSkip);
         Task<List<QuestionComment>> GetQuestionCommentsAsync(Guid postId,  int cntToSkip);
+        Task<PostComment> GetPostCommentByIdAsync(Guid commentId);
+        Task<QuestionComment> GetQuestionCommentByIdAsync(Guid commentId);
     }
 }
