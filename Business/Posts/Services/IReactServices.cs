8fff53a34294a3b6eb24bb2c87fe0820bed72f35
mahmoudklaled <mahmoud.khaled19640@gmail.com>
2023-08-11 21:45:04 +0300

    retrun reacts  object on add

--- Business/Posts/Services/IReactServices.cs
+++ Business/Posts/Services/IReactServices.cs
@@ -1,4 +1,5 @@
-﻿using DomainModels.Models;
+﻿using DomainModels.DTO;
+using DomainModels.Models;
 
 
 namespace Business.Posts.Services
@@ -9,10 +10,10 @@ namespace Business.Posts.Services
         Task<bool> DeleteQuestionPostReactAsync(Guid reactId, string userEmail);
         Task<bool> DeleteCommentPostReactAsync(Guid reactId, string userEmail);
         Task<bool> DeleteCommentQuestionReactAsync(Guid reactId, string userEmail);
-        Task<bool> AddReactOnPostAsync(AddReactRequest reactRequest, string userEmail);
-        Task<bool> AddReactOnQuestionPostAsync(AddReactRequest reactRequest, string userEmail);
-        Task<bool> AddReactOnPostCommentAsync(AddReactRequest reactRequest, string userEmail);
-        Task<bool> AddReactOnQuestionPostCommentAsync(AddReactRequest reactRequest, string userEmail);
+        Task<(bool, Guid)> AddReactOnPostAsync(AddReactRequest reactRequest, string userEmail);
+        Task<(bool, Guid)> AddReactOnQuestionPostAsync(AddReactRequest reactRequest, string userEmail);
+        Task<(bool, Guid)> AddReactOnPostCommentAsync(AddReactRequest reactRequest, string userEmail);
+        Task<(bool, Guid)> AddReactOnQuestionPostCommentAsync(AddReactRequest reactRequest, string userEmail);
         Task<bool> UpdatePostReact(ReactUpdateRequest reactRequest, string userEmail);
         Task<bool> UpdateQuestionReact(ReactUpdateRequest reactRequest, string userEmail);
         Task<bool> UpdatePostCommentReact(ReactUpdateRequest reactRequest, string userEmail);
@@ -21,6 +22,11 @@ namespace Business.Posts.Services
         Task<List<DomainModels.DTO.ReactsDTO>> GetQuestionCommentReacts(Guid questionPostCommentId);
         Task<List<DomainModels.DTO.ReactsDTO>> GetPostReacts(Guid postId);
         Task<List<DomainModels.DTO.ReactsDTO>> GetQuestionReacts(Guid questionId);
+        Task<ReactsDTO> GetReactByIdOnQuestionComment(Guid reactId);
+        Task<ReactsDTO> GetReactByIdOnQuestionPost(Guid reactId);
+        Task<ReactsDTO> GetReactByIdOnPostComment(Guid reactId);
+        Task<ReactsDTO> GetReactByIdOnPost(Guid reactId);
+        
 
     }
 }
--- Business/Posts/Services/ReactsServices.cs
+++ Business/Posts/Services/ReactsServices.cs
@@ -16,11 +16,11 @@ namespace Business.Posts.Services
             _unitOfWork = unitOfWork;
         }
 
-        public async Task<bool> AddReactOnPostAsync(AddReactRequest reactRequest, string userEmail)
+        public async Task<(bool,Guid)> AddReactOnPostAsync(AddReactRequest reactRequest, string userEmail)
         {
             var user = await _unitOfWork.UserAccounts.FindAsync(p => p.Email == userEmail);
             var post = await _unitOfWork.Post.FindAsync(p => p.Id == reactRequest.ObjectId);
-            if(user == null || post==null) return false;
+            if(user == null || post==null) return (false,Guid.Empty);
             var react = new PostReact
             {
                 Id = Guid.NewGuid(),
@@ -29,14 +29,14 @@ namespace Business.Posts.Services
                 UserAccountsId = user.Id
             };
             await _unitOfWork.PostReact.AddAsync(react);
-            return await _unitOfWork.Complete() > 0;
+            return  (await _unitOfWork.Complete() > 0,react.Id);
         }
 
-        public async Task<bool> AddReactOnPostCommentAsync(AddReactRequest reactRequest, string userEmail)
+        public async Task<(bool, Guid)> AddReactOnPostCommentAsync(AddReactRequest reactRequest, string userEmail)
         {
             var user = await _unitOfWork.UserAccounts.FindAsync(p => p.Email == userEmail);
             var Comment = await _unitOfWork.PostComment.FindAsync(p => p.Id == reactRequest.ObjectId);
-            if (user == null || Comment == null) return false;
+            if (user == null || Comment == null) return (false, Guid.Empty);
             var react = new PostCommentReact
             {
                 Id = Guid.NewGuid(),
@@ -45,14 +45,14 @@ namespace Business.Posts.Services
                 UserAccountsId = user.Id
             };
             await _unitOfWork.PostCommentReact.AddAsync(react);
-            return await _unitOfWork.Complete() > 0;
+            return (await _unitOfWork.Complete() > 0, react.Id);
         }
 
-        public async Task<bool> AddReactOnQuestionPostAsync(AddReactRequest reactRequest, string userEmail)
+        public async Task<(bool, Guid)> AddReactOnQuestionPostAsync(AddReactRequest reactRequest, string userEmail)
         {
             var user = await _unitOfWork.UserAccounts.FindAsync(p => p.Email == userEmail);
             var post = await _unitOfWork.QuestionPost.FindAsync(p => p.Id == reactRequest.ObjectId);
-            if (user == null || post == null) return false;
+            if (user == null || post == null) return (false, Guid.Empty);
             var react = new QuestionReact
             {
                 Id = Guid.NewGuid(),
@@ -61,14 +61,14 @@ namespace Business.Posts.Services
                 UserAccountsId = user.Id
             };
             await _unitOfWork.QuestionReact.AddAsync(react);
-            return await _unitOfWork.Complete() > 0;
+            return (await _unitOfWork.Complete() > 0, react.Id);
         }
 
-        public async Task<bool> AddReactOnQuestionPostCommentAsync(AddReactRequest reactRequest, string userEmail)
+        public async Task<(bool, Guid)> AddReactOnQuestionPostCommentAsync(AddReactRequest reactRequest, string userEmail)
         {
             var user = await _unitOfWork.UserAccounts.FindAsync(p => p.Email == userEmail);
             var Comment = await _unitOfWork.QuestionComment.FindAsync(p => p.Id == reactRequest.ObjectId);
-            if (user == null || Comment == null) return false;
+            if (user == null || Comment == null) return (false, Guid.Empty);
             var react = new QuestionCommentReact
             {
                 Id = Guid.NewGuid(),
@@ -77,7 +77,7 @@ namespace Business.Posts.Services
                 UserAccountsId= user.Id
             };
             await _unitOfWork.QuestionCommentReact.AddAsync(react);
-            return await _unitOfWork.Complete() > 0;
+            return (await _unitOfWork.Complete() > 0, react.Id);
         }
 
         public async Task<bool> DeleteCommentPostReactAsync(Guid reactId, string userEmail)
@@ -149,6 +149,35 @@ namespace Business.Posts.Services
             string[] includes = { "UserAccounts" };
             var reacts = await _unitOfWork.QuestionPost.FindAllAsync(r => r.Id == questionId, includes);
             var reactDTO = OMapper.Mapper.Map<List<ReactsDTO>>(reacts);
+            return reactDTO;
+        }
+
+        public async Task<ReactsDTO> GetReactByIdOnQuestionPost(Guid reactId)
+        {
+            string[] includes = { "UserAccounts" };
+            var react = await _unitOfWork.QuestionReact.FindAllAsync(r => r.Id == reactId, includes);
+            var reactDTO = OMapper.Mapper.Map<ReactsDTO>(react);
+            return reactDTO;
+        }
+        public async Task<ReactsDTO> GetReactByIdOnPost(Guid reactId)
+        {
+            string[] includes = { "UserAccounts" };
+            var react = await _unitOfWork.PostReact.FindAllAsync(r => r.Id == reactId, includes);
+            var reactDTO = OMapper.Mapper.Map<ReactsDTO>(react);
+            return reactDTO;
+        }
+        public async Task<ReactsDTO> GetReactByIdOnPostComment(Guid reactId)
+        {
+            string[] includes = { "UserAccounts" };
+            var react = await _unitOfWork.PostCommentReact.FindAllAsync(r => r.Id == reactId, includes);
+            var reactDTO = OMapper.Mapper.Map<ReactsDTO>(react);
+            return reactDTO;
+        }
+        public async Task<ReactsDTO> GetReactByIdOnQuestionComment(Guid reactId)
+        {
+            string[] includes = { "UserAccounts" };
+            var react = await _unitOfWork.QuestionCommentReact.FindAllAsync(r => r.Id == reactId, includes);
+            var reactDTO = OMapper.Mapper.Map<ReactsDTO>(react);
             return reactDTO;
         }
 
